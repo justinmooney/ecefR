98a450c1b5dd604b01d5a91b618a131519151de5
justinmooney <bombay56@gmail.com>
2016-04-21 16:00:41 -0500

    add another test. for science.

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,9 +1,16 @@
 library(ecefR)
 context("Number Checking")
 
-test_that("ECEF2LLA is correct", {
+test_that("Numbers are correct", {
     ecef <- c(-1472.238, 5005.461, 3692.592)
     lla <- c(35.46943, 106.39, 20.9998)
     expect_equal(xyz2lla(ecef), lla, tolerance = 1e-6)
     expect_equal(lla2xyz(lla), ecef, tolerance = 1e-6)
+
+    ecef <- c(123, 456, 789)
+    lla <- c(60.2444, 74.90448, -5442.65383)
+    expect_equal(xyz2lla(ecef), lla, tolerance = 1e-6)
+    expect_equal(lla2xyz(lla), ecef, tolerance = 1e-6)
 })
+
+
